56f67cc0e7a8eb77333322afa61d324ff5a910e4
sujeethyadav21 <sujeethnallavelli21@gmail.com>
2026-04-08 02:35:38 +0000

    Added MongoDB connection and submit route

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client.todo_db
+collection = db.items
+
 @app.route('/')
 def home():
-    return "Welocome to my To-Do App!"
-@app.route('/api')
-def api():
-    return jsonify({"message": "Hello World"})
+    return render_template('index.html')
+
+# This is the "Door" that fixes your 404 error!
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Get data from the HTML form
+    name = request.form.get("itemName")
+    description = request.form.get("itemDescription")
+    
+    # Save it to MongoDB
+    collection.insert_one({
+        "itemName": name,
+        "itemDescription": description
+    })
+    
+    return "Success! Item added to the database."
 
 if __name__ == "__main__":
     app.run(debug=True)
